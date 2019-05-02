e8946b13b683a92109746f71d5bbd0378b57acdd
CesarRivasP <cesarjose58@gmail.com>
2019-05-02 16:11:08 -0400

    Added listenner message for client

--- public/js/socket-custom.js
+++ public/js/socket-custom.js
@@ -2,9 +2,12 @@ var socket = io();
 
 socket.on('connect', function(){
   console.log('Conectado al servidor');
-})
-
+});
 
 socket.on('disconnect', function(){
   console.log('Se perdio la conexion con el servidor');
-})
+});
+
+socket.on('sendMessage', function(data){
+  console.log(data);
+});
