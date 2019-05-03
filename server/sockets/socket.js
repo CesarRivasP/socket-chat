a50eb905b84ecf42cd03bf1a90421fc671875946
CesarRivasP <cesarjose58@gmail.com>
2019-05-02 20:27:11 -0400

    Addded createMessageListener and others changes

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -2,6 +2,7 @@ const { io } = require('../server');
 const { Users } = require('../classes/users');
 const users = new Users();
 
+const { createMessage } = require('../utils/utilities');
 
 io.on('connection', (client) => {
 
@@ -17,6 +18,32 @@ io.on('connection', (client) => {
 
     let persons = users.addPersons(client.id, data.name);
 
-    callback(persons) //personas conectadas al chat
+    client.broadcast.emit('listPersons',  users.getPersons());
+
+    callback(persons); //personas conectadas al chat
+  })
+
+  client.on('createMessage', (data) => {
+    let person = users.getPerson(client.id);
+
+    // let message = createMessage(data.name, data.message);
+    let message = createMessage(person.name, data.message);
+
+    client.broadcast.emit('createMessage', message)
+  })
+
+  client.on('disconnect', () => {
+    // Asi se resuelve la duplicidad de los usuarios
+    let personDelete = users.deletePerson(client.id);
+
+    // client.broadcast.emit('createMessage', { BEFORE
+    //   user: 'Admin',
+    //   message: `${personDelete.name} abandono el chat`
+    // })
+
+    // AFTER
+    client.broadcast.emit('createMessage', createMessage('Admin', `${personDelete.name} abandono el chat`))
+
+    client.broadcast.emit('listPersons',  users.getPersons());
   })
 })
